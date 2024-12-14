7ebff8bcf5daa88565fd576e16c02a5c6d5c0409
taranvd <apple.vlad.taran@gmail.com>
2024-12-15 01:55:27 +0200

    feat: add tests for RevenueSharing contract functionality
    - Added tests for adding participants with valid share percentages.
    - Added tests for distributing revenue correctly among participants.
    - Verified that contract logic works as expected with proper share allocation.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@typechain/hardhat";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: "0.8.28", // версія вашого контракту
+  paths: {
+    artifacts: "./artifacts",
+  },
+  typechain: {
+    outDir: "typechain", // зберігає згенеровані типи у цій директорії
+    target: "ethers-v6", // використовує "ethers-v5" для генерації типів
+  },
 };
 
 export default config;
